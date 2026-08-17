f979469d6d15b07f5063badd561ab33f31c19134
Justin Lau <jlau11150@gmail.com>
2026-08-16 19:52:31 -0700

    Completed exercise 6 of javascript-exercise foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let stringRepeat = "";
+        for (let i = 0; i < num; i++) {
+            stringRepeat += str
+        }
+        return stringRepeat;
+    }
 
 };
+
+// links that helped me understand and this exercise done
+// https://stackoverflow.com/questions/45149893/javascript-loop-to-return-string-x-times
+// https://forum.freecodecamp.org/t/basic-algorithm-scripting-repeat-a-string-repeat-a-string/426797
 
 // Do not edit below this line
 module.exports = repeatString;

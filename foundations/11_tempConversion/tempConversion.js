e2aac74b9abe066d0842168dfca52d7438a5a3d4
Justin Lau <jlau11150@gmail.com>
2026-08-18 15:27:04 -0700

    Completed exercise 11 of javascript-exercises of foundations!

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversionC = 5/9 * (temp - 32)
+  let rounded = parseFloat(conversionC.toFixed(1))
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversionF = (9/5 * temp) + 32
+  let rounded = parseFloat(conversionF.toFixed(1))
+  return rounded
 };
 
 // Do not edit below this line

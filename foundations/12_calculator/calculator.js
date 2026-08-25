98f83a1b263352c658e6b9b33ac0b6c430739dad
Justin Lau <jlau11150@gmail.com>
2026-08-25 14:40:16 -0700

    Completed javascript-excerise 12 of TOP foundations

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumOfArray = 0
+	for (const num of arr) {
+    sumOfArray += num
+  }
+  return sumOfArray
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+// props to Geeks4Geeks for the multiply one: https://www.geeksforgeeks.org/javascript/multiply-the-elements-of-an-array-in-javascript/
+
+const power = function(base, exponent) {
+	return base ** exponent 
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res = 1;
+  for (let i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res
 };
+
+// again props to Geeks for factorial one as well: https://www.geeksforgeeks.org/javascript/factorial-of-a-number-using-javascript/
 
 // Do not edit below this line
 module.exports = {

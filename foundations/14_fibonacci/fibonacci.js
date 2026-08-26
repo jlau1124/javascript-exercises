ce047fa260d537b5dfb773e249ab583933d2ffc9
Justin Lau <jlau11150@gmail.com>
2026-08-26 11:37:14 -0700

    Completed exercise 14 of javascrip-exercises TOP foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let currentValue = 1
+  let nextValue = 1
+  if (Number(n) === 1 || Number(n) === 2) {
+    return 1
+  } else if (Number(n) === 0) {
+    return 0
+  }
+    else if (n < 0) {
+    return "OOPS"
+  } else {
+    for (let i = 3; i <= Number(n); i++) {
+      let nextNumber = currentValue + nextValue
+      currentValue = nextValue
+      nextValue = nextNumber
+    }
+  }
+  return nextValue
 };
+
+// converting string to a #: https://stackoverflow.com/questions/1133770/how-can-i-convert-a-string-to-an-integer-in-javascript
 
 // Do not edit below this line
 module.exports = fibonacci;

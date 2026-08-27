3effe858a599fc77022b914f01899b51b4eaeaac
Justin Lau <jlau11150@gmail.com>
2026-08-26 18:54:43 -0700

    Completed exercise 16 of javascript-exercises TOP foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let age;
+  
+    for (const obj of arr) {
+        if (Object.hasOwn(obj, "yearOfDeath")) {
+        age = obj["yearOfDeath"] - obj["yearOfBirth"]
+        obj["age"] = age;
+        } else {
+        let currentYear = (new Date()).getFullYear();
+        age = currentYear - obj["yearOfBirth"]
+        obj["age"] = age;
+        }
+    }
+    
+    const oldest = arr.sort(function(a,b) {
+    if (a.age > b.age) {
+        return -1
+    } else {
+        return 1
+    }
+    })
 
+    return oldest[0]
 };
 
 // Do not edit below this line
